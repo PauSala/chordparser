c32ae3440c177d00da3d8186e2543affa10f6163
pausala <pau.salaf@gmail.com>
2024-11-10 19:22:17 +0100

    fix: benchmark

--- benches/bench_parser.rs
+++ benches/bench_parser.rs
@@ -3,14 +3,19 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
 fn parse(n: &str, parser: &mut Parser) {
-    let result = parser.parse(&n);
-    result.unwrap();
+    let res = parser.parse(&n);
+    match res {
+        Ok(_) => (),
+        Err(e) => {
+            dbg!(e);
+        }
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut parser = Parser::new();
     c.bench_function("C", |b| {
-        b.iter(|| parse(black_box("C#+Maj76omit59#11"), black_box(&mut parser)))
+        b.iter(|| parse(black_box("CMaj7"), black_box(&mut parser)))
     });
 }
 
