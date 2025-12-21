e2cec4375d4512bb1f821d0b59bf2c0f70f714f2
pausala <pau.salaf@gmail.com>
2025-12-21 20:47:33 +0100

    make norm_intervals private

--- src/chord/mod.rs
+++ src/chord/mod.rs
@@ -37,7 +37,7 @@ pub struct Chord {
     pub intervals: Vec<Interval>,
     /// The normalized intervals of the notes, used to normalize the name
     #[serde(skip_serializing)]
-    pub norm_intervals: Vec<Interval>,
+    norm_intervals: Vec<Interval>,
     /// The semantic intervals of the notes, meaning non altered intervals.
     #[serde(skip_serializing)]
     semantic_intervals: Vec<u8>,
